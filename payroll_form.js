5a83b36c681bc7d595ad97bfeb02ea444ea98383
Siddhant <santoshshrivastava39@gmail.com>
2022-05-07 15:37:31 +0530

    Employee payroll

--- payroll_form.js
+++ payroll_form.js
@@ -25,6 +25,53 @@ try {
    
 });
 
+const save=()=>{
+    alert(salary.tostring())
+    try{
+        let employeePayrollData=CreateEmployeePayroll();
+    }catch(e)
+    {
+        return;
+    }
+}
+ const CreateEmployeePayroll=()=>
+ {
+     let employeePayrollData=new EmployeePayrollData();
+     try{
+         employeePayrollData.name=getInputValueById('#name');
+     }catch(e)
+     {
+         setTextValue('.text-error',e);
+         throw e;
+     }
+     employeePayrollData.profilePic=getSelectedValues('[name=profile]').pop();
+    employeePayrollData.gender=getSelectedValues('[name=gender]').pop();
+    employeePayrollData.department = getSelectedValues ('[name=department]'); 
+    employeePayrollData.salary = getInputValueById('#salary');
+    employeePayrollData.note=getInputValueById('#notes');
+    let date = getInputValueById('#day') +" " +getInputValueById('#month') +" "
+    +getInputValueById('#year');
+    employeePayrollData.date=Date.parse(date);
+    alert(employeePayrollData.toString());
+    return employeePayrollData;
+ }
+
+ const getInputValueById=(id)=> {
+     let value = document.queryselector(id).value;
+     return value;
+ }
+
+ const  getSelectedValues = (propertyValue) => {
+
+    let allItems=document.querySelectorAll(propertyValue);
+    let selItems=[];
+    allItems.forEach(item =>{
+        if(item.checked) selItems.push(item.value);
+
+    });
+    return selItems;
+}
+
 
 
 
